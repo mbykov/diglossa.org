10bbfb8969d03be033751423189351898bdd5bfe
Michael Bykov <m.bykov@gmail.com>
2022-04-30 13:43:46 +0300

    init

---
+++ src/pages/index.svelte
@@ -0,0 +1,50 @@
+<script>
+  import { onMount } from 'svelte'
+  import jsonview from '@pgrabovets/json-view'
+
+  onMount(async () => {
+		// const res = await fetch(`/tutorial/api/album`);
+		// photos = await res.json();
+    const data = '{"name": "json-view","version": "1.0.0"}'
+    const tree = jsonview.create(data)
+    jsonview.render(tree, document.querySelector('#anthrax-results'))
+    jsonview.expand(tree)
+	});
+
+
+</script>
+
+<h1 class="text-xl class:mx-32 px-32 text-pink-500">App?! ===</h1>
+<p>A short introduction would have been nice here...</p>
+
+<p id="anthrax-results" contenteditable="true" class="px-8">editable</p>
+
+
+<a href="/guide">guide</a>
+
+<nav class="bg-blue-900 shadow-lg">
+  <div class="container mx-auto">
+    <div class="sm:flex">
+      <a href="#" class="text-white text-3xl font-bold p-3">APP LOGO</a>
+
+      <!-- Menus -->
+      <div class="ml-55 mt-4">
+        <ul class="text-white sm:self-center text-xl">
+          <li class="sm:inline-block">
+            <a href="#" class="p-3 hover:text-red-900 ">About</a>
+          </li>
+          <li class="sm:inline-block">
+            <a href="#" class="p-3 hover:text-red-900">Services</a>
+          </li>
+          <li class="sm:inline-block">
+            <a href="#" class="p-3 hover:text-red-900">Blog</a>
+          </li>
+          <li class="sm:inline-block">
+            <a href="#" class="p-3 hover:text-red-900">Contact</a>
+          </li>
+        </ul>
+      </div>
+
+    </div>
+  </div>
+</nav>
---
+++ vite.config.js
@@ -0,0 +1,62 @@
+const { appConfig } = require('./package.json')
+const viteMainJs = require('vite-main-js')
+const slug = require('remark-slug')
+const tailwind = require('tailwindcss')
+const postcssImport = require('postcss-import')
+const { mdsvex } = require('mdsvex')
+const autoPreprocess = require('svelte-preprocess')
+const { svelte } = require('@sveltejs/vite-plugin-svelte')
+const { port } = appConfig
+const production = process.env.NODE_ENV === 'production'
+module.exports = {
+  server: {
+    port: port,
+  },
+  build: {
+    polyfillModulePreload: false,
+    cssCodeSplit: false,
+  },
+  optimizeDeps: {
+    exclude: ['@roxi/routify'],
+  },
+  resolve: {
+    dedupe: ['@roxi/routify'],
+  },
+  plugins: [
+    viteMainJs(),
+    svelte({
+      preprocess: [
+        mdsvex({
+          remarkPlugins: [slug],
+          layout: {
+            blog: 'src/components/Card.svelte',
+          },
+          extension: 'md',
+        }),
+        autoPreprocess({
+          postcss: {
+            plugins: [
+              tailwind({
+                mode: 'jit',
+                darkMode: 'class',
+                future: {
+                  removeDeprecatedGapUtilities: true,
+                  purgeLayersByDefault: true,
+                },
+                plugins: [],
+                purge: {
+                  content: ['./src/**/*.svelte'],
+                  enabled: production,
+                },
+              }),
+              postcssImport,
+            ],
+          },
+        }),
+      ],
+      emitCss: true,
+      hot: !production,
+      extensions: ['.md', '.svelte'],
+    }),
+  ],
+}
