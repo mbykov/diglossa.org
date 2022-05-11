533c116d4ae572fa859d529518910a04bbe23f0f
Michael Bykov <m.bykov@gmail.com>
2022-05-11 22:17:00 +0300

    v.1.0.0

--- src/pages/index.svelte
+++ src/pages/index.svelte
@@ -9,12 +9,18 @@
 		// const res = await fetch(`/tutorial/api/album`);
 		// photos = await res.json();
     const data = '{"name": "json-view","version": "1.0.0"}'
-    const tree = jsonview.create(data)
+    let tree = jsonview.create(data)
     jsonview.render(tree, document.querySelector('#anthrax-results'))
     jsonview.expand(tree)
 
     // let anthrax = await import('/home/michael/greek/anthrax')
+    let wf = 'ἀθλητής'
+    let res = await anthrax(wf)
+    console.log('_R', res)
 
+    tree = jsonview.create(res)
+    jsonview.render(tree, document.querySelector('#anthrax-results'))
+    jsonview.expand(tree)
 
 
     // https://github.com/mbykov/anthrax
@@ -27,7 +33,8 @@
 <h1 class="text-xl px-4 text-pink-500">App?! ===</h1>
 <p>A short introduction would have been nice here...</p>
 
-<p id="anthrax-results" contenteditable="true" class="px-8">editable</p>
+<!-- <p id="anthrax-results" contenteditable="true" class="px-8">editable</p> -->
+<p id="anthrax-results" class="px-8">editable</p>
 
 
 <!-- <a href="/guide">guide</a> -->
