6ccbd5baa45a5d85f0639169ed99be8f871e9f05
Michael Bykov <m.bykov@gmail.com>
2022-05-15 13:53:19 +0300

    v.1.0.0

--- src/pages/_layout.svelte
+++ src/pages/_layout.svelte
@@ -4,34 +4,41 @@
   import Lang from '../components/Lang.svelte'
 </script>
 
-<div id="app">
-  <aside>
+<div id="app" class="flex h-screen w-screen_ mt-4_">
+  <aside class="w-64 h-screen m-4">
     <div><Lang /></div>
     <NavBar />
   </aside>
-  <section>
+
+  <section class="flex-grow_  ">
     <header />
-    <main class="container px-1">
+    <main class="container px-1_ h-screen_">
       <slot />
     </main>
     <footer />
   </section>
+
+  <aside class="w-64">
+    &nbsp;
+  </aside>
+
 </div>
 
 <style>
-  #app {
+  #app_ {
     display: grid;
     grid-template-columns: 196px auto 196px ;
     height: 100%;
     width: 100%;
     position: absolute;
   }
-  main {
+  main_ {
     padding-top: 24px;
-  }
-  aside {
-    padding: 16px;
     box-shadow: 0 0 32px rgba(0, 0, 0, 0.4);
+  }
+  aside_ {
+    padding: 16px;
+    /* box-shadow: 0 0 32px rgba(0, 0, 0, 0.4); */
     height: 100%;
   }
 
