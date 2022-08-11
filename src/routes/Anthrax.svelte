f6bd5f8af3b45c5c714a7dd9fc6576ff380fb368
Michael Bykov <m.bykov@gmail.com>
2022-08-12 00:49:56 +0300

    v.1.1.1

--- src/routes/Anthrax.svelte
+++ src/routes/Anthrax.svelte
@@ -1,12 +1,37 @@
 <script>
  export let chains
  export let wf
- let kuku = chains
- console.log('_anthrax', kuku)
+
+ // title: seg-seg-seg
+ // wf --------> dname
+ // morph
+ // trns
+
+  function segs(chain) {
+    return chain.map(seg=> seg.seg).join('-')
+  }
+
+  function trns(chain) {
+    let cdict = chain.slice(-2)[0].cdict
+    if (!cdict) return []
+    let trns = cdict.trns
+    return trns
+  }
+
 </script>
 
-<div class="">
-  Anthrax nested
-  wf: {wf}
-  chains: {chains}
+<div class="h-screen_">
+    Anthrax
+    <p>wf: {wf}</p>
+
+    {#each chains as chain}
+      <p>segs: {segs(chain)}</p>
+      <p>trns: {trns(chain)}</p>
+    {(console.log( '_chain_', { chain }), '')}
+    {/each}
+
+</div>
+
+<div class="mt-8 h-screen_">
+    <!-- {(console.log( '_chains', { chains }), '')} -->
 </div>
