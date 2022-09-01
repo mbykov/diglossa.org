fc27d74e20a38bdde09d83df4c8d0f17819eeaff
Michael Bykov <m.bykov@gmail.com>
2022-09-01 19:09:25 +0300

    v.1.1.1

--- .svelte-kit/generated/client-manifest.js
+++ .svelte-kit/generated/client-manifest.js
@@ -6,6 +6,7 @@ export const components = [
 	() => import("../../src/routes/Anthrax.svelte"),
 	() => import("../../src/routes/[wf].svelte"),
 	() => import("../../src/routes/index.svelte"),
+	() => import("../../src/routes/widgets/Cdict.svelte"),
 	() => import("../../src/routes/widgets/Main.svelte"),
 	() => import("../../src/routes/widgets/Term.svelte")
 ];
@@ -13,7 +14,8 @@ export const components = [
 export const dictionary = {
 	"": [[0, 4], [1]],
 	"Anthrax": [[0, 2], [1]],
-	"widgets/Main": [[0, 5], [1]],
-	"widgets/Term": [[0, 6], [1]],
+	"widgets/Cdict": [[0, 5], [1]],
+	"widgets/Main": [[0, 6], [1]],
+	"widgets/Term": [[0, 7], [1]],
 	"[wf]": [[0, 3], [1], 1]
 };
