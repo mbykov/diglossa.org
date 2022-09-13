5b92bc088c3edd670afa970b4457c5679789bf88
Michael Bykov <m.bykov@gmail.com>
2022-09-13 19:26:08 +0300

    v.1.1.1

--- src/routes/[wf]/Segments.svelte
+++ src/routes/[wf]/Segments.svelte
@@ -1,14 +1,21 @@
 <script>
   export let segments
   $: console.log('_SGS', segments)
+
 </script>
 
-================ segments
-
-<div class="bg-white">
+<div class="bg-gray-100 px-4">
 
   {#each Object.entries(segments) as [segs, chain]}
-    <p>{segs}</p>
-{/each}
+      <p>
+      {#each chain as seg}
+        {#if (seg.cdicts || seg.pref)}
+          <span class="px-1 seg">{seg.seg}</span>
+        {:else}
+          <span class="px-1">{seg.seg}</span>
+        {/if}
+      {/each}
+      </p>
+  {/each}
 
 </div>
