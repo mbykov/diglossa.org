4b081a8f3e485a6615e52031460d2201cd2be22c
Michael Bykov <m.bykov@gmail.com>
2022-09-15 20:50:50 +0300

    v.1.1.1

---
+++ src/routes/[wf]/ClipContent.svelte
@@ -0,0 +1,8 @@
+<h1>Anthrax v. 2.0</h1>
+
+<p>Copy any greek text (utf-8) anywhere and paste it here with Ctrl-v</p>
+<p>Or try <span class="examples clickable">examples</span> section</p>
+<p> </p>
+<p> </p>
+<p> </p>
+<p> </p>
