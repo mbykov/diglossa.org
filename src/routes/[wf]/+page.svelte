2467148814d96a96b5ce78c2d19676f615dd7f53
Michael Bykov <m.bykov@gmail.com>
2022-09-24 15:30:09 +0300

    v.1.1.1

--- src/routes/[wf]/+page.svelte
+++ src/routes/[wf]/+page.svelte
@@ -2,46 +2,66 @@
 
   import { onMount } from 'svelte'
   import { goto } from '$app/navigation';
-  // import Anthrax from '../Anthrax.svelte'
-  // import Examples from '../Examples.svelte'
-  import Main from './Main.svelte'
-
-  // import Segments from './Segments.svelte'
-  import PrettyFLS from './PrettyFLS.svelte'
-  import Cdicts from './Cdicts.svelte'
-  import Forms from './Forms.svelte'
-  import ClipContent from './ClipContent.svelte'
-  import Cognates from './Cognates.svelte'
   import _ from 'lodash'
+
+  import ClipContent from './ClipContent.svelte'
+
+  import Main from './Main.svelte'
+  import NoResult from './NoResult.svelte'
+  import Cdicts from './Cdicts.svelte'
+
+  import Forms from './Forms.svelte'
+  import Cognates from './Cognates.svelte'
 
   const log = console.log
   let clipkey = ''
 
   export let data
-
-  let cognshow = false
-  let cognkey = {}
   $: console.log('_[WF]: data', data)
+  let wf = ''
+  let chains = []
+  let mainpop
 
-  $: cdicts = data.cdicts
-
-  $: wf = data.wf
-  $: chains = data.chains || []
-  $: console.log('_[WF]:', wf, '[CHAIN]', chains, '<=')
-
-  if (data.chains?.length) {
-    let chain = data.chains[0]
-    let mainseg = chain.find(seg=> seg.mainseg)
-    // $: console.log('_mainseg:', mainseg)
-    let cognates = mainseg ? mainseg.cognates : []
-    // $: console.log('_cognates:', cognates)
+  const addpops = {
+      forms: Forms,
+      cognates: Cognates
   }
 
- $: probe = cdicts.find(cdict=> cdict.dname == 'wkt') || cdicts[0]
-  // $: console.log('_PROBE', probe)
-  // $: console.log('_cdicts:', cdicts)
+  const mainpops = {
+      main: Main,
+      nores: NoResult
+  }
 
- /* $: cdicts = data.cdicts */
+  $: {
+      chains = data.chains
+      let chain = data.chains[0]
+      wf = data.wf
+      log('_XXX', wf, chain)
+      mainpop = (chain) ? mainpops.main : mainpops.nores
+  }
+
+  // let cognshow = false
+  // let cognkey = {}
+
+  //  $: cdicts = data.cdicts
+
+ //  $: wf = data.wf
+ //  $: chains = data.chains || []
+ //  $: console.log('_[WF]:', wf, '[CHAIN]', chains, '<=')
+
+ //  $: if (data.chains?.length) {
+ //    // let chain = data.chains[0] || []
+ //    // let mainseg = chain.find(seg=> seg.mainseg)
+ //    // // $: console.log('_mainseg:', mainseg)
+ //    // let cognates = mainseg ? mainseg.cognates : []
+ //    // // $: console.log('_cognates:', cognates)
+ //  }
+
+ // $: probe = cdicts.find(cdict=> cdict.dname == 'wkt') || cdicts[0]
+ //  // $: console.log('_PROBE', probe)
+ //  // $: console.log('_cdicts:', cdicts)
+
+ // /* $: cdicts = data.cdicts */
 
   function showDicts(seg) {
       let segment = seg.detail
@@ -50,7 +70,6 @@
       cognates = segment.cognates
   }
 
-  // let forms = []
   async function handleClick(ev) {
       let owf = ev.target
       if (!owf.classList.contains('wf')) return
@@ -106,7 +125,6 @@
   </div>
 </div>
 
-{#if (chains[0])}
 
 <div id="popup-morphs" class="popup absolute w-1/2 right-4 top-4 -my-4 h-screen p-4 pr-1">
   <div class="h-full bg-[#FAFAD2] shadow-2xl overflow-y-auto">
@@ -115,19 +133,20 @@
       <span class="dict">wkt</span> <span class="dict">dvr</span> <span class="esc w-1/3 text-right"> [x]</span>
     </div>
 
-    <Main {chains} {wf} on:segment={showDicts} />
+    <svelte:component this={mainpop} {wf} {chains}/>
 
-    <svelte:component this={Cdicts} {cdicts}  />
   </div>
 </div>
 
-{#if cognshow}
-{#key cognkey}
+
+{#if (false && cognshow)}
+  {#key (cognkey)}
 <Cognates {cognates} {wf} />
 {/key}
 {/if}
 
-{#key probe}
+{#if false}
+  {#key probe}
     <div id="popup-forms" class="hidden popup absolute w-1/4 right-4 top-4 -my-4 h-screen p-4 pr-1">
       <div class="h-full bg-[#E1E1C2] shadow-2xl overflow-y-auto">
         <div class="main-title text-right px-2">
