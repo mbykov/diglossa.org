fe42bf3d25504349109bfeeda838fe3ead0442b9
Michael Bykov <m.bykov@gmail.com>
2024-03-09 16:19:53 +0300

    v.1.1.1

--- src/routes/[wf]/+page.server.js
+++ src/routes/[wf]/+page.server.js
@@ -6,7 +6,7 @@ import { anthrax } from "@mbykov/anthrax"
 export const load = async ({ url, params }) => {
     const wf = params.wf
     let chains = await anthrax(wf)
-    // console.log('_CHAINS', chains)
+    console.log('_WF_SERVER_CHAINS', chains)
     return {
         chains,
         wf
---
+++ src/routes/[wf]/lib/Relatives.svelte
@@ -0,0 +1,16 @@
+<script>
+    // import Cdict from './Cdict.svelte'
+
+    // export let cdict
+
+</script>
+
+<div>
+    === Relatives ===
+    <!-- <div class="cdicts"> -->
+    <!--     {#each cdict.cdicts as cdict} -->
+    <!--         <Cdict {cdict} /> -->
+    <!--     {/each} -->
+    <!-- </div> -->
+
+</div>
