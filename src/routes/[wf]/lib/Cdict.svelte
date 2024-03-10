3753f62b971951b8c0c78de4ce7dbb2d0a190661
Michael Bykov <m.bykov@gmail.com>
2024-03-10 14:39:30 +0300

    v.1.1.1

--- src/routes/[wf]/lib/Cdict.svelte
+++ src/routes/[wf]/lib/Cdict.svelte
@@ -1,20 +1,36 @@
 <script>
     export let toggle = () => {}
     export let cdict
-    // export let pos
 
-    let pos = 'kuku'
+    // let pos = 'kuku'
+    $: pos = (cdict.name) ? 'name' : (cdict.verb) ? 'verb' : ''
+    if (cdict.gend) pos = [pos, cdict.gend].join('; ')
+    // console.log('_CDICT', cdict)
+
+    let greekUnicode = /[\u{0370}-\u{03FF}\u{1F00}-\u{1FFF}]/u
+    $: trns = cdict.trns.map(trn=> {
+        if (!trn) return ''
+        // trn.map(str=> {
+        //     let wfs = str.split(' ')
+        // })
+        return trn.replace(/([^\p{P} \n]+)[\u{0370}-\u{03FF}\u{1F00}-\u{1FFF}]/ug, " <span class=\"wf\">$1</span>")
+    })
+
 </script>
 
 <div class="cdict pb-4">
     <div class="flex pt-1">
-        <div class="w-1/2"> <b>dict</b>: {cdict.rdict} <span class="text-green-400">{pos}</span></div>
+        <div class="w-1/2"> <b>dict</b>: <span class="wf">{cdict.rdict}</span> <span class="text-green-400">{pos}</span></div>
         <div class="w-1/2 text-right text-green-600"> {cdict.dname}  </div>
     </div>
 
+    <!-- let html = row.replace(/([^\p{P} \n]+)/ug, " <span class=\"wf\">$1</span>") -->
+            <!-- {trn}<br> -->
+
     <div class="trns max-h-24 overflow-y-auto bg-gray-100 p-4" on:click="{toggle}">
-        {#each cdict.trns as trn}
-            {trn}<br>
+        {#each trns as trn}
+            {@html trn}<br>
+            <!-- {@html trn.replace(/([^\p{P} \n]+)/ug, " <span class=\"wf\">$1</span>")}<br> -->
         {/each}
     </div>
 
