19652d6a4c0de1566c686a73e18086e3b3e75117
Michael Bykov <m.bykov@gmail.com>
2025-01-26 13:07:59 +0300

    v.1.1.1

--- svelte.config.js
+++ svelte.config.js
@@ -8,7 +8,15 @@ const mdsvexOptions = {
 };
 /** @type {import('@sveltejs/kit').Config}*/
 const config = {
-  kit: {
+    onwarn: (warning, handler) => {
+        // suppress warnings on `vite dev` and `vite build`; but even without this, things still work
+        if (warning.code === "a11y-click-events-have-key-events") return;
+        if (warning.code === "a11y-no-static-element-interactions") return;
+        if (warning.code.startsWith('a11y-')) return;
+        if (/a11y-/.test(warning.code)) return;
+        handler(warning);
+    },
+    kit: {
     // adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
     // If your environment is not supported, or you settled on a specific environment, switch out the adapter.
     // See https://svelte.dev/docs/kit/adapters for more information about adapters.
