678b11648b9f8e208ee17bc0f5c15c80355b7c4f
Michael Bykov <m.bykov@gmail.com>
2025-02-01 23:10:48 +0300

    v.1.1.1

--- src/lib/ui/LeftHeader.svelte
+++ src/lib/ui/LeftHeader.svelte
@@ -1,14 +1,17 @@
 <script>
-    // import { page } from "$app/stores";
-    // import { base } from "$app/paths"
-    // import { goto } from '$app/navigation';
-	// import { get } from "svelte/store"
-    import { onMount } from 'svelte'
-
-    // import Transition from 'svelte-transition'
-
     import { Hamburger } from 'svelte-hamburgers';
     import { fly } from 'svelte/transition';
+
+    import { Drawer, Drawerhead, Button, uiHelpers } from 'svelte-5-ui-lib';
+    // import { InfoCircleSolid } from "flowbite-svelte-icons";
+    const drawer = uiHelpers();
+    let drawerStatus = $state(false);
+    const closeDrawer = drawer.close;
+
+    $effect(() => {
+        // drawerStatus = drawer.isOpen;
+        drawerStatus = open
+    });
 
     let open = $state(false);
 
@@ -16,14 +19,12 @@
 
     const log = console.log
 
-    // let routes = $state([])
-    // let menu = $state([])
     let { menu } = $props()
-    // log('____HEADER MENU', menu);
+
     let routes = $derived(menu[locale.current])
 
     function toggleNav(ev) {
-        open = false
+        if (open) open = !open
     }
 
 </script>
@@ -32,26 +33,25 @@
 
 <nav class="header px-4 relative bg-white" >
     <div class="h-14">
-    <Hamburger
-        bind:open
-        type="collapse"
-        --padding="12px"
-        title="Toggle nav links"
-        ariaControls="nav"
-        />
+        <Hamburger
+            bind:open
+            type="collapse"
+            --padding="12px"
+            title="Toggle nav links"
+            ariaControls="nav"
+            />
     </div>
+</nav>
 
-    {#if open}
-      <ul
-          id="nav"
-          transition:fly={{ y: -15 }}
-          class="menu absolute top-100 left-0 shadow-lg focus:outline-none w-48 dark:text-white bg-white p-4 shadow-lg "
-          >
 
-          {#each routes as route}
-            <li><a href="{route.href}" class="cursor-pointer hover:text-slate-500">{route.name}</a></li>
-          {/each}
+<Drawer drawerStatus={drawerStatus} closeDrawer={closeDrawer} backdrop={false} activateClickOutside={false} class="top-12 h-screen start-0">
 
-        </ul>
-      {/if}
-    </nav>
+    anthrax.js v.1.1.1
+
+    <ul class="focus:outline-none w-48 dark:text-white bg-white p-4">
+        {#each routes as route}
+          <li class="py-2"><a href="{route.href}" class="cursor-pointer hover:text-slate-500">{route.name}</a></li>
+        {/each}
+      </ul>
+
+</Drawer>
