c1c5d2cd39158064ac20bedf6442656d56dc29d2
Michael Bykov <m.bykov@gmail.com>
2025-03-04 21:39:39 +0300

    v.1.1.1

--- src/routes/[wf]/Chain.svelte
+++ src/routes/[wf]/Chain.svelte
@@ -9,6 +9,9 @@
     // let cont = $derived(data.conts[0])
     // log('______CONT.svelte cont', cont)
 
+    let contrels = $derived(cont.rels.map(rel=> ('<span class="rel cursor-pointer px-1_ ">' + rel + '</span>')).join(', ') )
+    let morerels = $derived(cont.morels.map(rel=> ('<span class="rel cursor-pointer px-1_ ">' + rel + '</span> ')).join(', ') )
+
     let showDicts = true
     let showRels = $state(false)
     let showMore = $state(false)
@@ -27,6 +30,15 @@
         let target = ev.target
         if (!target.classList.contains('rels-list')) return
         showRels = !showRels
+        // if (!showRels) showMore = false
+        showMore = false
+        log('_T', showRels, showMore)
+    }
+
+    function toggleMoreList(ev) {
+        let target = ev.target
+        if (!target.classList.contains('more-list')) return
+        showMore = !showMore
     }
 
     function toggleDict(ev) {
@@ -48,39 +60,46 @@
 </script>
 
 <!-- {#each Object.entries(cdict.trn) as [dname, trns]} -->
-<div class="anthrax-container">
+<div class="anthrax-container overflow-x-auto)">
 
-    <div class="dicts-list pt-2" onclick="{toggleDict}">
+    <div class="dicts-list pt-2 max-w-96_ overflow-x-scroll" onclick="{toggleDict}">
         <span class="text-green-400">dicts</span>:
         {#each cont.rdicts as rdict}
           <span class="rdict cursor-pointer px-1">{rdict}</span>
         {/each}
         <span class="text-green-400 cursor-pointer rels-list" onclick="{toggleRelsList}">relatives</span>:
-          {#if showRels}
-            {#each cont.rels as rdict}
-              <span class="px-1">{rdict}</span>
+        {#if showRels}
+          {@html contrels}
+          {#each cont.rels as rdict}
+            <!-- <span class="rel cursor-pointer px-1 "> {rdict} </span> -->
+          {/each}
+          <span class="px-1 text-green-400 cursor-pointer more-list" onclick="{toggleMoreList}"> more...</span>
+          {#if showMore}
+            {@html morerels}
+            {#each cont.morels as rdict}
+              <!-- <span class="morel cursor-pointer px-1"> {rdict} </span> -->
             {/each}
-          {:else}
-            {cont.rels.length}
           {/if}
-
-    </div>
+        {:else}
+          {cont.rels.length}
+        {/if}
+      </div>
 
     {#each cont.cdicts as chain, idx}
 
         <div class="translations rdict={chain.cdict.rdict} px-2" rdict={chain.cdict.rdict}>
-        <div class="cdict-head flex justify-between py-4">
-            <div class="cdict-rdict ">
-                dict: <span class="text-green-800 font-bold">{chain.cdict.rdict}</span>
-            </div>
-            <div class="cdict-morphs">
-                {#if chain.morphs}
-                  <Morphs {chain} />
-                {/if}
-              </div>
-        </div>
 
-        <!-- <div class="translations rdict={chain.cdict.rdict} px-2" rdict={chain.cdict.rdict}> -->
+            <div class="cdict-head flex justify-between py-4">
+                <div class="cdict-rdict ">
+                    dict: <span class="text-green-800 font-bold">{chain.cdict.rdict}</span>
+                </div>
+                <div class="cdict-morphs">
+                    {#if chain.morphs}
+                      <Morphs {chain} />
+                    {/if}
+                  </div>
+            </div>
+
             {#each chain.cdict.trns as trn}
               <div class="trns grow_ max-h-24 overflow-y-auto bg-gray-200 p-4 w-full my-2 " onclick="{toggle}">
                   <div class="text-green-500 font-bold float-right">{trn.dname}</div>
@@ -89,7 +108,7 @@
                       {#each trn.trns as row}
                         <p>{row}</p>
                       {/each}
-                  </div>
+                    </div>
 
               </div>
             {/each}
