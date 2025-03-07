c4abaf60494e2b02c5551534800e4c63eef0c302
Michael Bykov <m.bykov@gmail.com>
2025-03-07 20:42:26 +0300

    v1.1.1

--- src/routes/[wf]/+page.svelte
+++ src/routes/[wf]/+page.svelte
@@ -1,12 +1,10 @@
 <script>
 
-    import { page } from '$app/state';
+    // import { page } from '$app/state';
     import Chain from './Chain.svelte'
     import Schemes from './Schemes.svelte'
-    import { onMount } from 'svelte'
-    import { odicts } from "$lib/shared.svelte";
-    import _ from 'lodash'
-    import Morphs from './Morphs.svelte'
+    // import { odicts } from "$lib/shared.svelte";
+    // import _ from 'lodash'
 
     const log = console.log
 
@@ -14,46 +12,11 @@
     let { data } = $props()
 
     let wf = $derived(data.wf)
-    let dnames = _.compact(odicts.current.map(dict=> dict.active ? dict.key : false))
+    // let dnames = _.compact(odicts.current.map(dict=> dict.active ? dict.key : false))
+    $inspect('_+page_data.wf', wf);
 
     let conts = $derived(data.conts)
-    // $inspect('_page_conts', conts);
-
-    let showDicts = true
-    let showRels = $state(false)
-    let showMore = $state(false)
-    // onMount(async () => {
-    // })
-
-    // function toggle(ev) {
-    //     let target = ev.target
-    //     let otrns = target.closest('.trns')
-    //     if (!otrns) return
-    //     otrns.classList.toggle('overflow-y-auto')
-    //     otrns.classList.toggle('max-h-24')
-    // }
-
-    // // function toggleRelsList(ev) {
-    //     let target = ev.target
-    //     if (!target.classList.contains('rels-list')) return
-    //     showRels = !showRels
-    // }
-
-    // function toggleDict(ev) {
-    //     let target = ev.target
-    //     if (!target.classList.contains('rdict')) return
-    //     let wf = target.textContent
-    //     log('_xxx DICT', wf)
-    //     let otobehidden = document.querySelectorAll('.translations')
-    //     let selector = ['[rdict="', wf, '"]'].join('')
-    //     // let tobeshown = document.querySelectorAll('[rdict="ὄκλασμα"]')
-    //     let otobeshown = document.querySelectorAll(selector)
-    //     // log('_xxx DICT', tobehidden)
-    //     // log('_xxx DICT', tobeshown)
-    //     otobehidden.forEach(o=> o.classList.add('hidden'))
-    //     otobeshown.forEach(o=> o.classList.remove('hidden'))
-    // }
-
+    $inspect('_+page_conts', conts);
 
     function showSegment(ev) {
         if (!ev.target.classList.contains('segment')) return
@@ -76,7 +39,7 @@
       <div> waiting...</div>
     {:then}
       {#each conts as cont}
-        <Chain {cont} />
+        <Chain {cont} {wf} />
       {/each}
     {:catch error}
 	  <p style="color: red">{error.message}</p>
