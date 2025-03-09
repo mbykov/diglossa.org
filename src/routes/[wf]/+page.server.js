c9437058f7025ab34409ede643fb3156759cb87a
Michael Bykov <m.bykov@gmail.com>
2025-03-09 21:03:20 +0300

    v.1.1.1

--- src/routes/[wf]/+page.server.js
+++ src/routes/[wf]/+page.server.js
@@ -17,7 +17,7 @@ export const load = async ({ url, params }) => {
     // log('_WF_SERVER_dnames', dnames)
 
     let conts = await anthrax(wf)
-    log('____+ps, conts', conts)
+    // log('____+ps, conts', conts)
 
     if (!conts) {
         log('_no_conts')
@@ -27,32 +27,53 @@ export const load = async ({ url, params }) => {
         }
     }
 
+    for (let cont of conts) {
+        log('____+ps, cdicts', cont.cdicts)
+    }
+
     let dictkeys = _.flatten(conts.map(cont=> cont.cdicts.map(cdict=> cdict.dict)))
     dictkeys = _.uniq(dictkeys)
     console.log('____dictkeys', dictkeys)
 
-    let alltdicts = await getTrns(dictkeys) // , dnames
-    let rtrns = alltdicts.map(cdict=> cdict.rdict)
+    // let trnsdicts = await getTrns(dictkeys) // , dnames
+    // let rtrns = trnsdicts.map(cdict=> cdict.rdict)
     // console.log('____rtrns', rtrns)
 
     let cachedicts = await getCacheD(dictkeys)
-    console.log('____cachedicts', cachedicts.length)
+    console.log('____cachedicts', cachedicts)
+    let rcacheDs = cachedicts.map(cdict=> cdict.rdict)
+    console.log('____rcacheDs', rcacheDs)
+
+    // for (let cont of conts) {
+    //     for (let cdict of cont.cdicts) {
+    //         cdict.show = true
+    //         if (cont.indecl) continue
+    //         cdict.trns = []
+    //         let tdicts = trnsdicts.filter(tdict=> tdict.dict == cdict.dict && tdict.pos == cdict.pos)
+    //         for (let tdict of tdicts) {
+    //             cdict.trns.push({dname: tdict.dname, trns: tdict.trns})
+    //             // log('_____KKKKK', tdict.dname, tdict.trns)
+    //         }
+    //     }
+    // }
 
     for (let cont of conts) {
         for (let cdict of cont.cdicts) {
             cdict.show = true
             if (cont.indecl) continue
+            if (cont.pos == 'adverb') continue
             cdict.trns = []
-            let tdicts = alltdicts.filter(tdict=> tdict.dict == cdict.dict && tdict.pos == cdict.pos)
-            for (let tdict of tdicts) {
-                cdict.trns.push({dname: tdict.dname, trns: tdict.trns})
-                // log('_____KKKKK', tdict.dname, tdict.trns)
-            }
+            let tdict = cachedicts.find(tdict=> tdict.rdict == cdict.rdict && tdict.pos == cdict.pos)
+            // log('__________________TDict', cdict.rdict, cdict.pos, tdict)
+            if (!tdict) continue
+            // let trns = (tdict) ? tdict.trns : []
+            cdict.trns = tdict.trns
         }
     }
 
     // console.log('____trns_conts', conts)
 
+    // conts = []
     return {
         wf,
         conts
