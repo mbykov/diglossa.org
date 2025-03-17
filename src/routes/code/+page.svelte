5d34da43e9722123bf2dca51af06f567a1e7638a
Michael Bykov <m.bykov@gmail.com>
2025-03-17 19:29:59 +0300

    v.1.1.1

--- src/routes/code/+page.svelte
+++ src/routes/code/+page.svelte
@@ -1,3 +1,19 @@
-<p>
-    CODE =================
-</p>
+<div class="p-8">
+
+    <h2 class="text-green-600 font-bold p-4"> License: </h2>
+
+    <p>
+        <a href="https://www.gnu.org/licenses/gpl-3.0.en.html#license-text" target="blank"> GNU GENERAL PUBLIC LICENSE</a>
+    </p>
+
+    <h2 class="text-green-600 font-bold p-4"> Code: </h2>
+
+    <p>
+        <a href="https://github.com/mbykov/diglossa" target="blank"> https://github.com/mbykov/diglossa.org</a>
+    </p>
+
+    <p>
+        <a href="https://github.com/mbykov/anthrax" target="blank"> https://github.com/mbykov/anthrax</a>
+    </p>
+
+</div>
---
+++ src/routes/contacts/+page.svelte
@@ -0,0 +1,9 @@
+<div class="p-8">
+
+    <h2 class="text-green-600 font-bold"> Contacts: </h2>
+
+    <p>
+        <a href="http://t.me/diglossa_org" target="blank"> t.me/diglossa_org</a>
+    </p>
+
+</div>
